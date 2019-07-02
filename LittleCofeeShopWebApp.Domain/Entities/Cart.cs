75fb84d9d654d986d7136d88a82b75e4abd9186d
Shvidkoy Mychailo <origami690@gmail.com>
2019-07-02 19:38:12 +0300

    Create domain cart class and enter initial data

---
+++ LittleCofeeShopWebApp.Domain/Entities/Cart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleCofeeShopWebApp.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Recipe recipe, int quantity)
+        {
+
+        }
+        public void RemoveLine(Recipe product)
+        {
+
+        }
+
+        public decimal ComputeTotalValue()
+        {
+           // return lineCollection.Sum(e => e.Product.Price * e.Quantity);
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Recipe Recipe { get; set; }
+        public int Quantity { get; set; }
+    }
+}
