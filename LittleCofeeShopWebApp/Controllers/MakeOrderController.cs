69925c83316be94fcf2e79a28b06d13cdd182349
Shvidkoy Mychailo <origami690@gmail.com>
2019-07-02 17:36:05 +0300

    Added controller for making order and action for returning a partial view.

---
+++ LittleCofeeShopWebApp/Controllers/MakeOrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LittleCofeeShopWebApp.Controllers
+{
+    public class MakeOrderController : Controller
+    {
+        // GET: MakeOrder
+       /* public ActionResult MakeOrder(int cofeeId)
+        {
+            return View();
+        }
+        */
+        public PartialViewResult MakeOrder(int cofeeId)
+        {
+            return PartialView(cofeeId);
+        }
+    }
+}
